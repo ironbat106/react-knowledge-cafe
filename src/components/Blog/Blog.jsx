0b7fb0284eb5d24854abbf1505b9fd29c2902de3
ironbat106 <aryanahadateeq02@gmail.com>
2024-10-22 22:58:57 +0600

    remove bookmark after read and deploy to surge

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
-    const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
+    const {id, title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div className="mb-20  space-y-4">
             <img className="w-full mb-8" src={cover} alt={`Cover picture of the title ${title}`} />
@@ -25,7 +25,7 @@ const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
                 hashtags.map((hash, idx) => <span key={idx}><a href=""> {hash}</a></span>)
             }
            </p>
-           <button onClick={() => handleMarkAsRead(reading_time)} className="text-purple-600 font-bold underline">Mark as Read</button>
+           <button onClick={() => handleMarkAsRead(id, reading_time)} className="text-purple-600 font-bold underline">Mark as Read</button>
         </div>
     );
 };
